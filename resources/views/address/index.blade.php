ca8de8f026342b4624c24f59f15d2579adcd1ee3
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-01-31 13:26:38 +0700

    Menambahkan UI untuk menampilkan semua alamat

---
+++ resources/views/address/index.blade.php
@@ -0,0 +1,75 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Alamat') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+
+                    <div class="flex justify-end items-center">
+                        <x-primary-button type="button">
+                            <a href="{{route('address.create')}}">
+                                {{ __("Tambah") }}
+                            </a>
+                        </x-primary-button>
+                    </div>
+
+                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
+                        @foreach($addresses as $address)
+                            <div class="flex justify-between max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                                <div class="flex-1">
+                                    <h5 class="mb-2 inline-flex items-center space-x-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                                            <path fill-rule="evenodd" d="M9.69 18.933l.003.001C9.89 19.02 10 19 10 19s.11.02.308-.066l.002-.001.006-.003.018-.008a5.741 5.741 0 00.281-.14c.186-.096.446-.24.757-.433.62-.384 1.445-.966 2.274-1.765C15.302 14.988 17 12.493 17 9A7 7 0 103 9c0 3.492 1.698 5.988 3.355 7.584a13.731 13.731 0 002.273 1.765 11.842 11.842 0 00.976.544l.062.029.018.008.006.003zM10 11.25a2.25 2.25 0 100-4.5 2.25 2.25 0 000 4.5z" clip-rule="evenodd" />
+                                        </svg>
+                                        <span>{{ $address->name }}</span>
+                                    </h5>
+                                    <p class="font-normal text-gray-700 dark:text-gray-400">{{ $address->detail }}</p>
+                                </div>
+
+                                <x-dropdown>
+                                    <x-slot name="trigger">
+                                        <button>
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                                            </svg>
+                                        </button>
+                                    </x-slot>
+                                    <x-slot name="content">
+                                        <x-dropdown-link :href="route('address.edit', $address)">
+                                            {{ __('Edit') }}
+                                        </x-dropdown-link>
+                                        <form method="POST" action="{{ route('address.destroy', $address) }}">
+                                            @csrf
+                                            @method('delete')
+                                            <x-dropdown-link :href="route('address.destroy', $address)" onclick="event.preventDefault(); this.closest('form').submit();">
+                                                {{ __('Delete') }}
+                                            </x-dropdown-link>
+                                        </form>
+                                    </x-slot>
+                                </x-dropdown>
+                            </div>
+                        @endforeach
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Menampilkan Toast -->
+    @if(session('status'))
+        <div class="absolute flex items-center w-full max-w-xs p-4 space-x-4 text-gray-500 bg-white divide-x divide-gray-200 rounded-lg shadow right-5 bottom-5 dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800"
+             role="alert"
+             x-data="{ show: true }"
+             x-show="show"
+             x-transition
+             x-init="setTimeout(() => show = false, 2000)">
+            <div class="text-sm text-gray-600 dark:text-gray-400">{{ session('status') }}</div>
+        </div>
+    @endif
+</x-app-layout>
