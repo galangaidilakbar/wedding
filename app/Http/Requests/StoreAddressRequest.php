7a2d688569868c6cf0857d26848c94d75e080062
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-01-31 13:26:56 +0700

    Menambahkan custom request untuk menyimpan data alamat

---
+++ app/Http/Requests/StoreAddressRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreAddressRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:50',
+            'detail' => 'required|string|max:255',
+            'latitude' => 'nullable|between:-90,90',
+            'longitude' => 'nullable|between:-180,180',
+            'accuracy' => 'nullable|numeric',
+        ];
+    }
+}
