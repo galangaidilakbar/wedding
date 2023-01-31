8e78ca9d54aedbc75eb0b63593b50e44ad0734ed
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-01-31 13:24:13 +0700

    Menambahkan route resource address

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AddressController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('address', AddressController::class);
 });
 
 require __DIR__.'/auth.php';
