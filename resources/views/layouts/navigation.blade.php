81de81e1dd293bda149d357777ecf602d016c66b
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-01-31 13:25:07 +0700

    Menambahkan navigasi ke address

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -37,6 +37,9 @@
                         <x-dropdown-link :href="route('profile.edit')">
                             {{ __('Profile') }}
                         </x-dropdown-link>
+                        <x-dropdown-link :href="route('address.index')">
+                            {{ __('Alamat') }}
+                        </x-dropdown-link>
 
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
@@ -82,6 +85,10 @@
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
                     {{ __('Profile') }}
+                </x-responsive-nav-link>
+
+                <x-responsive-nav-link :href="route('address.index')">
+                    {{ __('Alamat') }}
                 </x-responsive-nav-link>
 
                 <!-- Authentication -->
