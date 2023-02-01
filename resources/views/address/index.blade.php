7d338a75aa939452cf9c09d060ceaceb6818fbab
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-02-01 09:58:07 +0700

    Menampilkan keterangan

--- resources/views/address/index.blade.php
+++ resources/views/address/index.blade.php
@@ -22,13 +22,25 @@
                         @foreach($addresses as $address)
                             <div class="flex justify-between max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                                 <div class="flex-1">
+
                                     <h5 class="mb-2 inline-flex items-center space-x-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
                                             <path fill-rule="evenodd" d="M9.69 18.933l.003.001C9.89 19.02 10 19 10 19s.11.02.308-.066l.002-.001.006-.003.018-.008a5.741 5.741 0 00.281-.14c.186-.096.446-.24.757-.433.62-.384 1.445-.966 2.274-1.765C15.302 14.988 17 12.493 17 9A7 7 0 103 9c0 3.492 1.698 5.988 3.355 7.584a13.731 13.731 0 002.273 1.765 11.842 11.842 0 00.976.544l.062.029.018.008.006.003zM10 11.25a2.25 2.25 0 100-4.5 2.25 2.25 0 000 4.5z" clip-rule="evenodd" />
                                         </svg>
                                         <span>{{ $address->name }}</span>
                                     </h5>
+
                                     <p class="font-normal text-gray-700 dark:text-gray-400">{{ $address->detail }}</p>
+
+                                    @if($address->keterangan)
+                                        <div class="inline-flex items-center space-x-2 text-xs text-gray-500 dark:text-gray-400">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-3 h-3">
+                                                <path d="M3.5 2.75a.75.75 0 00-1.5 0v14.5a.75.75 0 001.5 0v-4.392l1.657-.348a6.449 6.449 0 014.271.572 7.948 7.948 0 005.965.524l2.078-.64A.75.75 0 0018 12.25v-8.5a.75.75 0 00-.904-.734l-2.38.501a7.25 7.25 0 01-4.186-.363l-.502-.2a8.75 8.75 0 00-5.053-.439l-1.475.31V2.75z" />
+                                            </svg>
+                                            <p>{{ $address->keterangan }}</p>
+                                        </div>
+                                    @endif
+
                                 </div>
 
                                 <x-dropdown>
