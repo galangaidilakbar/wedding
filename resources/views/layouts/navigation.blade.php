fe159837e8715d896b0218d6de4f7e8c57b9b58a
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-02-02 14:42:04 +0700

    Menambahkan link ke products

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('product.index')" :active="request()->routeIs('product.*')">
+                        {{ __('Manajemen Produk') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -72,6 +75,10 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('product.index')" :active="request()->routeIs('product.index')">
+                {{ __('Manajemen Produk') }}
             </x-responsive-nav-link>
         </div>
 
