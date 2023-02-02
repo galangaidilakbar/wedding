1ff6976f31450ac29d93dadbfdf0ae3a1714e112
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-02-02 14:41:38 +0700

    Menambahkan route ke product resource

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AddressController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -28,7 +29,10 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::resource('address', AddressController::class);
+    Route::resources([
+        'address' => AddressController::class,
+        'product' => ProductController::class
+    ]);
 });
 
 require __DIR__.'/auth.php';
