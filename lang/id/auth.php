bf4d6e3220363e3df8424150de7531843f71ea29
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-02-05 17:10:19 +0700

    Menambahkan lokalisasi bahasa indonesia

---
+++ lang/id/auth.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'failed' => 'Kredensial ini tidak cocok dengan catatan kami.'
+];
---
+++ lang/id/translations.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'email' => 'Email',
+    'password' => 'Kata Sandi',
+    'Remember me' => 'Ingat Saya',
+    'Forgot your password?' => 'Lupa kata sandi?',
+    'Log in' => 'Masuk'
+];
