8fdea839967c4e11dc926c647da959208c4ad649
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-02-24 05:01:25 +0700

    Add user anita to seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,14 +13,20 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
         // \App\Models\User::factory(10)->create();
 
-         \App\Models\User::factory()->create([
+         User::factory()->create([
              'name' => 'Admin',
              'email' => 'admin@kodegakure.com',
              'is_admin' => true
+         ]);
+
+         User::factory()->create([
+             'name' => 'Anita Rahmawati',
+             'email' => 'anita@kodegakure.com',
+             'is_admin' => false
          ]);
     }
 }
