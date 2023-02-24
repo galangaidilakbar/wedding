d72c7372e139ea146df7fa29a4066cf4cfc12781
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-02-24 07:53:52 +0700

    Memperbaiki map, cara mendapatkan lokasinya, dan handle errornya.

--- resources/views/address/create.blade.php
+++ resources/views/address/create.blade.php
@@ -16,18 +16,6 @@
 
                         <section class="mt-6">
                             <div id="map" class="h-96"></div>
-
-                            <button class="mt-1 inline-flex items-center space-x-1 text-sm text-gray-600 dark:text-gray-400"
-                                    type="button"
-                                    onclick="getCurrentLocation()">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                                     class="w-5 h-5">
-                                    <path fill-rule="evenodd"
-                                          d="M9.69 18.933l.003.001C9.89 19.02 10 19 10 19s.11.02.308-.066l.002-.001.006-.003.018-.008a5.741 5.741 0 00.281-.14c.186-.096.446-.24.757-.433.62-.384 1.445-.966 2.274-1.765C15.302 14.988 17 12.493 17 9A7 7 0 103 9c0 3.492 1.698 5.988 3.355 7.584a13.731 13.731 0 002.273 1.765 11.842 11.842 0 00.976.544l.062.029.018.008.006.003zM10 11.25a2.25 2.25 0 100-4.5 2.25 2.25 0 000 4.5z"
-                                          clip-rule="evenodd"/>
-                                </svg>
-                                <span>{{__('Gunakan lokasi saat ini')}}</span>
-                            </button>
                         </section>
 
                         <form method="post" action="{{ route('address.store') }}" class="mt-6 space-y-6">
@@ -56,7 +44,8 @@
                                 <x-text-input id="keterangan" name="keterangan" type="text" class="mt-1 block w-full"
                                               :value="old('keterangan')"/>
                                 <x-input-error class="mt-2" :messages="$errors->get('keterangan')"/>
-                                <span class="text-xs text-gray-600 dark:text-gray-400">{{__('Tambahkan ini biar kami lebih mudah menemukan alamat mu')}}</span>
+                                <span
+                                    class="text-xs text-gray-600 dark:text-gray-400">{{__('Tambahkan ini biar kami lebih mudah menemukan alamat mu')}}</span>
                             </div>
 
                             <div class="flex items-center gap-4">
@@ -70,36 +59,34 @@
     </div>
 
     <script>
-        const map = L.map('map').setView([-1.933, 115.203], 4);
+        const map = L.map('map').fitWorld()
 
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            attribution: '© OpenStreetMap'
         }).addTo(map);
 
-        const options = {
-            enableHighAccuracy: true,
-            timeout: 5000,
-            maximumAge: 0
-        };
+        map.locate({setView: true, maxZoom: 16});
 
-        function success(pos) {
-            const crd = pos.coords;
-            map.flyTo([crd.latitude, crd.longitude], 19);
-            const marker = L.marker([crd.latitude, crd.longitude]).addTo(map);
-            marker.bindPopup(`Tingkat akurasi ${crd.accuracy} meter.`).openPopup();
+        function onLocationFound(e) {
+            const radius = e.accuracy;
 
-            document.getElementById("latitude").value = `${crd.latitude}`
-            document.getElementById("longitude").value = `${crd.longitude}`
-            document.getElementById("accuracy").value = `${crd.accuracy}`
+            L.marker(e.latlng).addTo(map)
+                .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+            L.circle(e.latlng, radius).addTo(map);
+
+            document.getElementById("latitude").value = e.latlng.lat
+            document.getElementById("longitude").value = e.latlng.lng
+            document.getElementById("accuracy").value = e.accuracy
         }
 
-        function error(err) {
-            console.warn(`ERROR(${err.code}): ${err.message}`);
+        map.on('locationfound', onLocationFound);
+
+        function onLocationError(e) {
+            alert(e.message);
         }
 
-        function getCurrentLocation() {
-            navigator.geolocation.getCurrentPosition(success, error, options);
-        }
+        map.on('locationerror', onLocationError);
     </script>
 </x-app-layout>
