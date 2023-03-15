94ad5b977a2962b7b5a263c14840679f1660b16b
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-03-15 13:21:11 +0700

    Memperbaiki routing address (sesuai kebutuhan)

--- routes/web.php
+++ routes/web.php
@@ -28,9 +28,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('address', AddressController::class)->only(['create', 'store', 'edit', 'update', 'destroy']);
 
     Route::resources([
-        'address' => AddressController::class,
         'product' => ProductController::class
     ]);
 });
