fbedfc2bec7e1ae8bb9375cf27d3621bb22e3f7f
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-03-15 13:48:48 +0700

    Menambahkan relation

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Address extends Model
 {
@@ -11,7 +12,7 @@ class Address extends Model
 
     protected $guarded = ['id'];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
