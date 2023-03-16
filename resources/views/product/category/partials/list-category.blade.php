b336e7cc810292bb1832dc9b26127285f4488270
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-03-16 18:08:12 +0700

    Membuat tampilan kategori

---
+++ resources/views/product/category/partials/list-category.blade.php
@@ -0,0 +1,66 @@
+<section>
+    <header>
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100 flex-1">
+            {{ __('Semua Kategori') }}
+        </h2>
+
+        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+            {{ __("Menampilkan semua kategori produk.") }}
+        </p>
+    </header>
+
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-6">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    No.
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Nama Kategori
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Aksi
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            @forelse($categories as $category)
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {{ $loop->iteration }}
+                    </th>
+                    <td class="px-6 py-4">
+                        {{ $category->name }}
+                    </td>
+                    <td class="px-6 py-4 flex space-x-2">
+                        <x-primary-link href="{{ route('category.edit', $category) }}">
+                            Ubah
+                        </x-primary-link>
+
+                        <div class="border-l"></div>
+
+                        <form method="POST" action="{{ route('category.destroy', $category) }}">
+                            @csrf
+                            @method('delete')
+                            <x-primary-link href="{{ route('address.destroy', $category) }}"
+                                            onclick="event.preventDefault(); this.closest('form').submit();">
+                                Hapus
+                            </x-primary-link>
+                        </form>
+                    </td>
+                </tr>
+            @empty
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td colspan="3" class="px-6 py-4">
+                        <figure class="flex flex-col justify-center items-center">
+                            <img src="{{ asset('img/empty.svg') }}" alt="empty illustration" class="w-20 h-auto">
+                            <figcaption class="mt-4 text-gray-500 dark:text-gray-400 text-sm">Data kategori produk masih kosong</figcaption>
+                        </figure>
+                    </td>
+                </tr>
+            @endforelse
+            </tbody>
+        </table>
+    </div>
+</section>
