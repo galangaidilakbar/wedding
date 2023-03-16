81a53667987da1a9af70e7cc6bf22efe0d5ed377
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-03-16 17:59:37 +0700

    Menambahkan link ke kategori

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,8 +15,11 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('category.index')" :active="request()->routeIs('category.*')">
+                        {{ __('Kategori Produk') }}
+                    </x-nav-link>
                     <x-nav-link :href="route('product.index')" :active="request()->routeIs('product.*')">
-                        {{ __('Manajemen Produk') }}
+                        {{ __('Produk') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -74,8 +77,8 @@
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
 
-            <x-responsive-nav-link :href="route('product.index')" :active="request()->routeIs('product.index')">
-                {{ __('Manajemen Produk') }}
+            <x-responsive-nav-link :href="route('category.index')" :active="request()->routeIs('category.*')">
+                {{ __('Kategori Produk') }}
             </x-responsive-nav-link>
         </div>
 
