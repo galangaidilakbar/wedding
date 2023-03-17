a834a87dc742076e828a490ba6622b7f28f804ec
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-03-17 07:31:05 +0700

    Menggunakan komponent textarea

--- resources/views/address/create.blade.php
+++ resources/views/address/create.blade.php
@@ -46,7 +46,10 @@
                             <!-- Detail alamat -->
                             <div>
                                 <x-input-label for="detail" :value="__('Detail Alamat')"/>
-                                <textarea id="detail" name="detail" rows="4" class="block p-2.5 w-full mt-1 text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-indigo-500 dark:focus:border-indigo-500" placeholder="Tuliskan alamat secara lengkap dimulai dari nama jalan, nomer rumah, RT/RW, Nama Desa, Kecamatan, Kabupaten, dan Provinsi">{{ old('detail') }}</textarea>
+                                <x-textarea class="mt-1 block w-full"
+                                            id="detail"
+                                            name="detail"
+                                            placeholder="Tuliskan alamat secara lengkap dimulai dari nama jalan, nomer rumah, RT/RW, Nama Desa, Kecamatan, Kabupaten, dan Provinsi">{{ old('detail') }}</x-textarea>
                                 <x-input-error class="mt-2" :messages="$errors->get('detail')"/>
                             </div>
 
