4b53aa55666794e5e5231cbfc32acd20458afe61
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-03-17 07:27:34 +0700

    Memperbaiki kolom2 yang dibutuhkan

--- database/migrations/2023_02_01_132821_create_products_table.php
+++ database/migrations/2023_02_01_132821_create_products_table.php
@@ -15,11 +15,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('photo');
-            $table->string('photo_url');
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
             $table->string('name');
             $table->text('description');
             $table->integer('price');
+            $table->string('photo');
+            $table->string('photo_url');
             $table->timestamps();
         });
     }
