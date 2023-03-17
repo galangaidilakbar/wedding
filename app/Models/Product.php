3a8dcef41c95f5b031fd5c25ec85a672eba31bbf
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-03-17 07:28:23 +0700

    Membuat relasi One to One ke model category

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
