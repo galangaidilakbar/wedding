5e87054df8119d62224eab3d8da4476d22fab4dc
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-03-23 22:20:43 +0700

    Improve alt attribute for screen reader user

--- resources/views/product/product-card.blade.php
+++ resources/views/product/product-card.blade.php
@@ -3,7 +3,7 @@
         <div
             class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="#">
-                <img class="p-8 rounded-t-lg" src="{{ $product->photo_url }}" alt="product image" height="100"/>
+                <img class="p-8 rounded-t-lg" src="{{ $product->photo_url }}" alt="{{ $product->name }}"/>
             </a>
             <div class="px-5 pb-5">
                 <a href="{{ route('product.show', $product) }}">
