a1ba2acc28d934d07cb5210e570da3cbc8781e26
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-03-27 09:50:58 +0700

    Menambahkan model, migration, dan untuk detail order

---
+++ app/Models/DetailOrder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class DetailOrder extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
