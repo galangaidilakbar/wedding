2e88147b2d921bd806796e8ecfa714b4b0662123
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-03-27 09:51:08 +0700

    Menambahkan relasi ke detail order

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -22,4 +22,9 @@ class Product extends Model
     {
         return $this->hasMany(Cart::class);
     }
+
+    public function detail_orders(): HasMany
+    {
+        return $this->hasMany(DetailOrder::class);
+    }
 }
