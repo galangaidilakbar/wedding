ca607e71039578d8b07635b68700ccd2f99ba832
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-03-27 09:48:36 +0700

    Menambahkan required pada input tanggal

--- resources/views/order/partials/tanggal-acara.blade.php
+++ resources/views/order/partials/tanggal-acara.blade.php
@@ -6,7 +6,7 @@
     </header>
     <div class="max-w-xl">
         <x-input-label for="tanggal_acara" :value="__('Mohon memilih tanggal acara.')"/>
-        <x-text-input id="tanggal_acara" class="block mt-4 w-full" type="date" name="tanggal_acara"/>
+        <x-text-input id="tanggal_acara" class="block mt-4 w-full" type="date" name="tanggal_acara" required/>
         <x-input-error :messages="$errors->get('tanggal_acara')" class="mt-2"/>
     </div>
 </section>
