58351f433d907e4fd2cb3103950b7f2ab94a9a19
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-03-27 08:01:09 +0700

    Membuat form untuk checkout

---
+++ resources/views/order/create.blade.php
@@ -0,0 +1,39 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Checkout') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <form action="{{ route('order.store') }}" method="post" x-data="{ jenis_pembayaran: 'dp' }">
+                        @csrf
+
+                        <!-- Alamat -->
+                        @include('order.partials.address-for-order')
+
+                        <!-- Produk Dipesan -->
+                        @include('order.partials.products-for-order')
+
+                        <!-- Tanggal Acara -->
+                        @include('order.partials.tanggal-acara')
+
+                        <!-- Jenis Pembayaran -->
+                        @include('order.partials.jenis-pembayaran')
+
+                        <!-- Metode Pembayaran -->
+                        @include('order.partials.metode-pembayaran')
+
+                        <!-- Rincian Pesanan -->
+                        @include('order.partials.rincian-pesanan-in-order')
+
+                        <x-primary-button>{{ __('Buat Pesanan') }}</x-primary-button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
