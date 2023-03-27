0e49c2a3d767ea20d4fa9209ef5f3133a8422703
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-03-27 08:02:41 +0700

    Inputan tanggal acara

---
+++ resources/views/order/partials/tanggal-acara.blade.php
@@ -0,0 +1,12 @@
+<section class="mb-6">
+    <header>
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+            {{ __('Tanggal Acara') }}
+        </h2>
+    </header>
+    <div class="max-w-xl">
+        <x-input-label for="tanggal_acara" :value="__('Mohon memilih tanggal acara.')"/>
+        <x-text-input id="tanggal_acara" class="block mt-4 w-full" type="date" name="tanggal_acara"/>
+        <x-input-error :messages="$errors->get('tanggal_acara')" class="mt-2"/>
+    </div>
+</section>
