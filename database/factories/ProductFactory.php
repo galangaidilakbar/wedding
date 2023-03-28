626b31cc93758ff8c5cbe703018cd95584b45f2d
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-03-28 08:18:54 +0700

    Create ProductFactory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->words(3, true),
+            'description' => fake()->paragraphs(3, true),
+            'price' => fake()->numberBetween(200000, 20000000),
+            'photo' => fake()->imageUrl(),
+            'photo_url' => fake()->imageUrl(),
+        ];
+    }
+}
