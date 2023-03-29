82aa53431234813148a002795c31e4a0756f78b4
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-03-29 12:34:59 +0700

    Menambahkan icon keranjang belanja di samping nama pengguna.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -30,6 +30,12 @@
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
+                <x-nav-link :href="route('cart.index')" :active="request()->routeIs('cart.index*')">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                        <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+                    </svg>
+                </x-nav-link>
+
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
@@ -48,8 +54,8 @@
                             {{ __('translations.Profile') }}
                         </x-dropdown-link>
 
-                        <x-dropdown-link :href="route('cart.index')">
-                            {{ __('translations.Shopping Cart') }}
+                        <x-dropdown-link :href="route('order.index')">
+                            {{ __('translations.Orders') }}
                         </x-dropdown-link>
 
                         <!-- Authentication -->
