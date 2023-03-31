37179a358b8f433aa5c5881044efc6d76135544c
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-03-31 16:28:43 +0700

    Installing package Laravel Invoices

---
+++ lang/vendor/invoices/id/invoice.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Invoice Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'invoice'                => 'Faktur Penjualan',
+    'serial'                 => 'Serial No.',
+    'date'                   => 'Tanggal Faktur',
+    'seller'                 => 'Penjual',
+    'buyer'                  => 'Pembeli',
+    'address'                => 'Alamat',
+    'code'                   => 'Kode',
+    'vat'                    => 'VAT code',
+    'phone'                  => 'Phone',
+    'description'            => 'Deskripsi',
+    'units'                  => 'Satuan',
+    'quantity'               => 'Qty',
+    'price'                  => 'Harga',
+    'discount'               => 'Discount',
+    'tax'                    => 'Tax',
+    'sub_total'              => 'Sub total',
+    'total_discount'         => 'Total discount',
+    'taxable_amount'         => 'Taxable amount',
+    'total_taxes'            => 'Total taxes',
+    'tax_rate'               => 'Tax rate',
+    'total_amount'           => 'Jumlah Total',
+    'pay_until'              => 'Silahkan bayar sebelum',
+    'amount_in_words'        => 'Jumlah dalam kalimat',
+    'amount_in_words_format' => '%s %s and %s %s',
+    'notes'                  => 'Notes',
+    'shipping'               => 'Shipping',
+    'paid'                   => 'Paid',
+    'due'                    => 'Due',
+];
