b7f646f80b8927aa7cd870a0587c2c4055c2cc1c
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-03-31 14:33:35 +0700

    Membuat tabel dan model Payments.php
    Payments.php berelasi dengan Order.php

---
+++ database/migrations/2023_03_30_124159_create_payments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained()->cascadeOnDelete();
+            $table->integer('amount')->nullable();          // in rupiah
+            $table->string('proof_of_payment')->nullable(); // path to image
+            $table->string('proof_of_payment_url')->nullable(); // url to image
+            $table->enum('status', ['pending', 'verified', 'rejected'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
