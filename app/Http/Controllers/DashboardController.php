95bddd3715079f08e378bb0ca9786bbb9ee9fe54
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-04-07 23:26:08 +0700

    In this commit:
    1. Showing product by its category.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -10,6 +11,6 @@ class DashboardController extends Controller
 {
     public function index(): View
     {
-        return view('dashboard', ['products' => Product::all()]);
+        return view('dashboard', ['categories' => Category::all()]);
     }
 }
---
+++ resources/views/product/products-with-category.blade.php
@@ -0,0 +1,27 @@
+<section>
+    @foreach($categories as $category)
+        <header class="first:pt-0 pt-6">
+            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100 mb-6">{{ $category->name }}</h2>
+        </header>
+
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 place-content-center">
+            @foreach($category->products()->latest()->take(3)->get() as $product)
+                <div
+                    class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-900 dark:border-gray-700">
+                    <a href="{{ route('product.show', $product) }}">
+                        <img class="rounded-t-lg w-full h-72 object-cover" src="{{ $product->photo_url }}"
+                             alt="{{ $product->name }}" loading="lazy"/>
+                    </a>
+                    <div class="px-5 py-5">
+                        <a href="{{ route('product.show', $product) }}">
+                            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white mb-2 hover:underline">{{ $product->name }}</h5>
+                        </a>
+
+                        <h3 class="text-base font-bold text-gray-900 dark:text-white">Rp
+                            @rupiah($product->price)</h3>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    @endforeach
+</section>
