44804fd1a9bbaec35067a823ffd98527d36cf4c3
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-04-20 05:03:33 +0700

    Improve UI

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -9,31 +9,27 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             @forelse($orders as $order)
                 <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                    <div
-                        class="grid grid-cols-1 mt-6 first:mt-0 border dark:border-gray-700 rounded-lg shadow-sm px-6 py-4 space-y-6">
-
-                        <!-- Nomor pesanan & Tombol -->
+                    <div class="grid grid-cols-1 mt-6 first:mt-0 space-y-6">
+                        <!-- Header -->
                         <div class="flex justify-between items-center">
-                            <h2 class="text-lg font-bold text-gray-900 dark:text-gray-100">
-                                {{ __('NO. PESANAN: ') }} {{ $order->id }}
-                            </h2>
-                            <x-primary-link href="{{ route('order.show', $order) }}">
+                            <div class="flex space-x-4 justify-between items-center lg:justify-start w-full lg:w-auto">
+                                <div class="text-sm text-gray-500 dark:text-gray-400">
+                                    {{ $order->created_at->format('d M Y') }}
+                                </div>
+                                <div
+                                    class="bg-green-100 text-green-800 text-xs font-medium px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
+                                    {{ $order->status }}
+                                </div>
+                                <div class="text-sm text-gray-500 dark:text-gray-400 hidden lg:block">
+                                    {{ $order->id }}
+                                </div>
+                            </div>
+                            <x-primary-link href="{{ route('order.show', $order) }}" class="hidden lg:block text-sm">
                                 {{ __('Lihat Detail Pesanan') }}
                             </x-primary-link>
                         </div>
 
-                        <!-- Tanggal & Status -->
-                        <div class="flex space-x-4">
-                            <div class="text-sm text-gray-500 dark:text-gray-400">
-                                {{ __('translations.Order date') }}: <span
-                                    class="text-gray-900 dark:text-gray-100">{{ $order->created_at->format('d M Y') }}</span>
-                            </div>
-                            <div class="border-l dark:border-gray-700"></div>
-                            <div
-                                class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
-                                {{ $order->status }}
-                            </div>
-                        </div>
+                        <div class="border-b border-dashed dark:border-gray-700"></div>
 
                         <!-- Products -->
                         <div class="grid grid-cols-1">
@@ -41,14 +37,14 @@
                                 <div class="flex space-x-8 mt-4 first:mt-0">
                                     <div class="w-32">
                                         <img src="{{ $cart->product->photo_url }}" alt="{{ $cart->product->name }}"
-                                            class="rounded" loading="lazy">
+                                             class="rounded" loading="lazy">
                                     </div>
                                     <div class="flex-1">
-                                        <div class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                                        <div class="text-sm lg:text-base font-medium text-gray-900 dark:text-gray-100">
                                             {{ $cart->product->name }}
                                         </div>
                                     </div>
-                                    <div class="text-gray-600 dark:text-gray-400">
+                                    <div class="hidden lg:block text-gray-600 dark:text-gray-400">
                                         Rp @rupiah($cart->product->price)
                                     </div>
                                 </div>
@@ -57,12 +53,27 @@
 
                         <div class="border-b border-dashed dark:border-gray-700"></div>
 
-                        <!-- Total -->
-                        <div class="text-right text-gray-900 dark:text-gray-100">
-                            {{ __('Total Pesanan: ') }}
-                            <span class="text-lg font-semibold">
+                        <!-- Total transaction in dekstop-->
+                        <div class="hidden lg:block text-right">
+                            <div class="text-xs text-gray-500 dark:text-gray-400">
+                                {{ __('Total Pesanan: ') }}
+                            </div>
+                            <div class="font-bold text-gray-900 dark:text-gray-100">
                                 Rp @rupiah($order->total_harga)
-                            </span>
+                            </div>
+                        </div>
+
+                        <!-- Total transaction in mobile-->
+                        <div class="flex justify-between items-center lg:hidden">
+                            <div class="text-xs text-gray-500 dark:text-gray-400">
+                                {{ __('Total Pesanan: ') }}
+                                <div class="text-sm font-semibold">
+                                    Rp @rupiah($order->total_harga)
+                                </div>
+                            </div>
+                            <x-primary-link href="{{ route('order.show', $order) }}" class="text-sm">
+                                {{ __('Lihat Detail Pesanan') }}
+                            </x-primary-link>
                         </div>
                     </div>
                 </div>
