19849d0b1ec15fcdd2a267c9614d705d7984ff13
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-05-01 17:11:15 +0700

    Change form into link.

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -13,86 +13,84 @@
                         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                             <thead
                                 class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                                <tr>
-                                    <th scope="col" class="px-6 py-3">
-                                        No.
-                                    </th>
-                                    <th scope="col" class="px-6 py-3">
-                                        <span class="sr-only">Image</span>
-                                    </th>
-                                    <th scope="col" class="px-6 py-3">
-                                        Nama Produk
-                                    </th>
-                                    <th scope="col" class="px-6 py-3">
-                                        Harga
-                                    </th>
-                                    <th scope="col" class="px-6 py-3">
-                                        Aksi
-                                    </th>
-                                </tr>
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    No.
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    <span class="sr-only">Image</span>
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Nama Produk
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Harga
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Aksi
+                                </th>
+                            </tr>
                             </thead>
                             <tbody>
-                                @forelse($carts as $cart)
-                                    <tr
-                                        class="bg-white border-b dark:bg-gray-900 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                        <th scope="row"
-                                            class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                            {{ $loop->iteration }}
-                                        </th>
-                                        <td class="w-32 p-4">
-                                            <img src="{{ $cart->product->photo_url }}" alt="{{ $cart->product->name }}">
-                                        </td>
-                                        <td class="px-6 py-4">
-                                            {{ $cart->product->name }}
-                                        </td>
-                                        <td class="px-6 py-4">
-                                            Rp @rupiah($cart->product->price)
-                                        </td>
-                                        <td class="px-6 py-4">
-                                            <form method="POST" action="{{ route('cart.destroy', $cart) }}">
-                                                @csrf
-                                                @method('delete')
-                                                <x-primary-link href="{{ route('cart.destroy', $cart) }}"
-                                                    onclick="event.preventDefault(); this.closest('form').submit();">
-                                                    Hapus
-                                                </x-primary-link>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @empty
-                                    <tr
-                                        class="bg-white border-b dark:bg-gray-900 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                        <td colspan="6" class="px-6 py-4">
-                                            <figure class="flex flex-col justify-center items-center">
-                                                <img src="{{ asset('img/undraw_empty_cart.svg') }}"
-                                                    alt="empty illustration" class="w-20 h-auto">
-                                                <figcaption class="mt-4 text-gray-500 dark:text-gray-400 text-sm">
-                                                    Keranjang belanja masih kosong
-                                                </figcaption>
-                                            </figure>
-                                        </td>
-                                    </tr>
-                                @endforelse
-                                @if ($total_price > 0)
-                                    <tr
-                                        class="bg-white border-b dark:bg-gray-900 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                        <th scope="row"
-                                            class="px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white font-semibold">
-                                            Total
-                                        </th>
-                                        <td colspan="2"></td>
-                                        <td class="px-6 py-4 text-gray-900 dark:text-white font-semibold">
-                                            Rp @rupiah($total_price)
-                                        </td>
-                                        <td class="px-6 py-4">
-                                            <form action="{{ route('order.create')}}">
-                                                <x-primary-button>
-                                                    Checkout
-                                                </x-primary-button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endif
+                            @forelse($carts as $cart)
+                                <tr
+                                    class="bg-white border-b dark:bg-gray-900 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                    <th scope="row"
+                                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        {{ $loop->iteration }}
+                                    </th>
+                                    <td class="w-32 p-4">
+                                        <img src="{{ $cart->product->photo_url }}" alt="{{ $cart->product->name }}">
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        {{ $cart->product->name }}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        Rp @rupiah($cart->product->price)
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <form method="POST" action="{{ route('cart.destroy', $cart) }}">
+                                            @csrf
+                                            @method('delete')
+                                            <x-primary-link href="{{ route('cart.destroy', $cart) }}"
+                                                            onclick="event.preventDefault(); this.closest('form').submit();">
+                                                Hapus
+                                            </x-primary-link>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr
+                                    class="bg-white border-b dark:bg-gray-900 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                    <td colspan="6" class="px-6 py-4">
+                                        <figure class="flex flex-col justify-center items-center">
+                                            <img src="{{ asset('img/undraw_empty_cart.svg') }}"
+                                                 alt="empty illustration" class="w-20 h-auto">
+                                            <figcaption class="mt-4 text-gray-500 dark:text-gray-400 text-sm">
+                                                Keranjang belanja masih kosong
+                                            </figcaption>
+                                        </figure>
+                                    </td>
+                                </tr>
+                            @endforelse
+                            @if ($total_price > 0)
+                                <tr
+                                    class="bg-white border-b dark:bg-gray-900 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                    <th scope="row"
+                                        class="px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white font-semibold">
+                                        Total
+                                    </th>
+                                    <td colspan="2"></td>
+                                    <td class="px-6 py-4 text-gray-900 dark:text-white font-semibold">
+                                        Rp @rupiah($total_price)
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <x-primary-button-link href="{{ route('order.create') }}">
+                                            Checkout
+                                        </x-primary-button-link>
+                                    </td>
+                                </tr>
+                            @endif
                             </tbody>
                         </table>
                     </div>
