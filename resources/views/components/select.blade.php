29d6d6b20160ad89d4034c010f78a2705a194949
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-05-02 14:37:22 +0700

    Change bg color on dark mode

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,3 +1,3 @@
-<select {!! $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}>
+<select {!! $attributes->merge(['class' => 'bg-gray-50 border-gray-300 dark:border-gray-700 dark:bg-gray-700 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}>
     {{ $slot }}
 </select>
