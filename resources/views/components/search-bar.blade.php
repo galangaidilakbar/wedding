64da905420bc74d899bb4a4c04464fec23d9497d
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-05-02 14:37:44 +0700

    Add value request('search')

--- resources/views/components/search-bar.blade.php
+++ resources/views/components/search-bar.blade.php
@@ -17,7 +17,7 @@
         </div>
         <input type="text" name="search" id="search"
                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-               placeholder="{{ $placeholder }}" required>
+               placeholder="{{ $placeholder }}" required value="{{ request('search') }}">
     </div>
     <button type="submit"
             class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
