3b8c8234824dd5fc800664fb3bba1ef3e9df5ebf
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-05-08 17:00:06 +0700

    change id to uuid

--- app/Models/Payments.php
+++ app/Models/Payments.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -35,7 +36,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  */
 class Payments extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
 
     protected $guarded = ['id'];
 
--- database/migrations/2023_03_30_124159_create_payments_table.php
+++ database/migrations/2023_03_30_124159_create_payments_table.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('payments', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->foreignUuid('order_id')->constrained()->cascadeOnDelete();
             $table->string('proof_of_payment'); // path to image
             $table->string('proof_of_payment_url'); // url to image
