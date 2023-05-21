f5889372fd4f344bc0077f68f208c730f834b791
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-05-21 20:28:43 +0700

    Change id type from number to uuid

--- database/migrations/2023_01_15_140453_create_categories_table.php
+++ database/migrations/2023_01_15_140453_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->timestamps();
         });
