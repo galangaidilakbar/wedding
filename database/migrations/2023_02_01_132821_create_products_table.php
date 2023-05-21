5075446ecd5cf2f37dcb3a8ce0ad00733d6418e0
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-05-21 20:28:59 +0700

    Remove category_id field

--- database/migrations/2023_02_01_132821_create_products_table.php
+++ database/migrations/2023_02_01_132821_create_products_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
             $table->string('name');
             $table->text('description');
             $table->integer('price');
