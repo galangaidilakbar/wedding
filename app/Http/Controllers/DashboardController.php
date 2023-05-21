6ef72de0333068558ef7d92cff297fe1f1a9e936
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-05-21 20:42:00 +0700

    Dashboard now just return product

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,16 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
+use App\Models\Product;
 use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
     public function index(): View
     {
+        $products = Product::query();
+
+        if (request()->has('sort')) {
+            if (request()->sort === 'lowest_price') {
+                $products->orderBy('price');
+            } elseif (request()->sort === 'highest_price') {
+                $products->orderByDesc('price');
+            } elseif (request()->sort === 'newest') {
+                $products->orderByDesc('created_at');
+            } elseif (request()->sort === 'oldest') {
+                $products->orderBy('created_at');
+            }
+        }
+
         return view('dashboard', [
-            'categories' => Category::with('products')->get(),
-            'show_all_products' => false,
+            'products' => $products->inRandomOrder()->paginate()->withQueryString(),
         ]);
     }
 }
