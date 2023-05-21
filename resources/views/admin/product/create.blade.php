803fcead9cd598a90c56ed0492423a48955abe08
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-05-21 20:46:22 +0700

    Change select to checkbox of category

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -27,15 +27,6 @@
                             </div>
 
                             <div>
-                                <x-input-label for="category_id" :value="__('Kategori Produk')"/>
-                                <x-select name="category_id" id="category_id" class="mt-1 block w-full">
-                                    @foreach($categories as $category)
-                                        <option value="{{ $category->id }}">{{ $category->name }}</option>
-                                    @endforeach
-                                </x-select>
-                            </div>
-
-                            <div>
                                 <x-input-label for="price" :value="__('Harga')"/>
                                 <x-text-input id="price" name="price" type="number" class="mt-1 block w-full"
                                               :value="old('price')" onchange="rupiah(this.value)"/>
@@ -60,6 +51,27 @@
                                 hover:file:bg-indigo-100 dark:hover:file:bg-indigo-500"
                                        accept=".png, .jpeg, .jpg">
                                 <x-input-error class="mt-2" :messages="$errors->get('photo')"/>
+                            </div>
+
+                            <div>
+                                <h3 class="mb-4 font-semibold text-gray-900 dark:text-white">{{ __('Kategori') }}</h3>
+                                <ul class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                                    @foreach($categories as $category)
+                                        <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+                                            <div class="flex items-center pl-3">
+                                                <input id="{{ $category->id }}"
+                                                       name="categories[]"
+                                                       type="checkbox"
+                                                       value="{{ $category->id }}"
+                                                       class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                                <label for="{{ $category->id }}"
+                                                       class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                                                    {{ $category->name }}
+                                                </label>
+                                            </div>
+                                        </li>
+                                    @endforeach
+                                </ul>
                             </div>
 
                             <div class="flex items-center gap-4">
