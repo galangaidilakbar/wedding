9a4ccba308849132df648511d6bb442e7a67cb2e
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-05-21 20:40:06 +0700

    Change fake name to fake word

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -18,7 +18,7 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name,
+            'name' => fake()->word,
         ];
     }
 }
