0964d1b7373995487708f577db117135147f6440
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-05-23 13:21:19 +0700

    add route to dashboard when user is already login

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,12 +4,18 @@
     </div>
 
     <div class="flex justify-center items-center space-x-4 mt-6">
-        <x-primary-button-link href="{{ route('login') }}">
-            Login
-        </x-primary-button-link>
+        @auth
+            <x-primary-button-link href="{{ route('dashboard') }}">
+                Dashboard
+            </x-primary-button-link>
+            @else
+            <x-primary-button-link href="{{ route('login') }}">
+                Login
+            </x-primary-button-link>
 
-        <x-secondary-button-link href="{{ route('register') }}">
-            Register
-        </x-secondary-button-link>
+            <x-secondary-button-link href="{{ route('register') }}">
+                Register
+            </x-secondary-button-link>
+        @endauth
     </div>
 </x-guest-layout>
