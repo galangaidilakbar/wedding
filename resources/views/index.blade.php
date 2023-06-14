88c3b40794783a38152b2d6e10c483a907b1bf93
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-06-14 23:14:22 +0700

    Improve UI for landing page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,21 +1,88 @@
-<x-guest-layout>
-    <div class="text-center text-sm text-gray-500 dark:text-gray-400">
-        Selamat datang di {{ config('app.name') }}
-    </div>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('translations.Dashboard') }}
+        </h2>
+    </x-slot>
 
-    <div class="flex justify-center items-center space-x-4 mt-6">
-        @auth
-            <x-primary-button-link href="{{ route('dashboard') }}">
-                Dashboard
-            </x-primary-button-link>
-            @else
-            <x-primary-button-link href="{{ route('login') }}">
-                Login
-            </x-primary-button-link>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div class="flex justify-between items-center flex-wrap gap-4 mb-5">
+                        <!-- Search bar -->
+                        <div class="w-full lg:w-auto">
+                            <x-search-bar to="{{ route('products.search') }}" placeholder="Cari produk..."/>
+                        </div>
 
-            <x-secondary-button-link href="{{ route('register') }}">
-                Register
-            </x-secondary-button-link>
-        @endauth
+                        <!-- Filter by status-->
+                        <form
+                            action=""
+                            method="get"
+                            class="mt-2 lg:mt-0 w-full lg:w-auto"
+                            onchange="this.closest('form').submit()">
+
+                            <x-input-label
+                                for="sort"
+                                value="Sort"
+                                class="sr-only"/>
+
+                            <x-select
+                                id="sort"
+                                name="sort"
+                                class="block w-full"
+                                required>
+                                <option value="" disabled>Urutkan</option>
+                                <option
+                                    value="latest"
+                                    {{ request('sort') === 'latest' ? 'selected' : '' }}>
+                                    Terbaru
+                                </option>
+                                <option
+                                    value="oldest"
+                                    {{ request('sort') === 'oldest' ? 'selected' : '' }}>
+                                    Terlama
+                                </option>
+                                <option
+                                    value="highest_price"
+                                    {{ request('sort') === 'highest_price' ? 'selected' : '' }}>
+                                    Harga tertinggi
+                                </option>
+                                <option
+                                    value="lowest_price"
+                                    {{ request('sort') === 'lowest_price' ? 'selected' : '' }}>
+                                    Harga terendah
+                                </option>
+                            </x-select>
+                        </form>
+                    </div>
+
+                    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 place-content-center">
+                        @foreach($products as $product)
+                            <div
+                                class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-900 dark:border-gray-700">
+                                <a href="{{ route('products.show', $product) }}">
+                                    <img class="rounded-t-lg w-full h-72 object-cover" src="{{ $product->photo_url }}"
+                                         alt="{{ $product->name }}" loading="lazy"/>
+                                </a>
+                                <div class="px-5 py-5">
+                                    <a href="{{ route('products.show', $product) }}">
+                                        <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white mb-2 hover:underline">{{ $product->name }}</h5>
+                                    </a>
+
+                                    <h6 class="text-base font-bold text-gray-900 dark:text-white">
+                                        Rp @rupiah($product->price)
+                                    </h6>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+
+                    <div class="mt-5">
+                        {{ $products->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</x-guest-layout>
+</x-app-layout>
